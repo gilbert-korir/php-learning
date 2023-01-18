094c2010b0e8fd18709e8246bd43d9266cabe1ce
gilbert <gilbert@iprocu.re>
2023-01-18 11:57:49 +0300

    m

---
+++ connection.php
@@ -0,0 +1,15 @@
+<?php
+
+    $con = mysqli_connect('localhost','root','','phptutorials');
+
+    if(!$con)
+    {
+            echo 'Please Check Your Database Connection';
+    }
+
+
+
+
+
+
+?>
---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link href="https:fonts.googleapis.com/css?family=Ubuntu&display=swap" rel="stylesheet">
+    <title>Registration Form Using HTML and CSS3</title>
+</head>
+<body>
+
+        <div class = "signup-form">
+            <img src="user.png">
+            <form action="process.php" method="post">
+                <input type="text" placeholder="User Name" class="txt" name="UserName">
+                <input type="email" placeholder="Email" class="txt" name="Email">
+                <input type="password" placeholder="Password" class="txt" name="Password">
+                <input type="password" placeholder="Confirm Password" class="txt" name="Cpass">
+                <input type="submit" placeholder="Create Account" class="btn" name="btn-save">
+                <a href="#"> Already Have an Account</a>
+            
+            </form>  
+
+
+
+
+
+
+        </div>
+    
+</body>
+</html>
